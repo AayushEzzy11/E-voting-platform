c91aacd09cc48c794fa5fbdaffb1b8778ba9ef1e
AayushEzzy11 <122254642+AayushEzzy11@users.noreply.github.com>
2025-08-14 11:18:06 +0545

    Add files via upload

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'readddyevotecode',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'readddyevotecode',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
